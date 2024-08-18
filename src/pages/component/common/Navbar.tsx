362551c093a440dffaa86dda2d240c01da73249a
Pakorn Sukmankong <floksong.001@gmail.com>
2024-08-18 23:16:11 +0700

    init project

---
+++ src/pages/component/common/Navbar.tsx
@@ -0,0 +1,11 @@
+import { Button, Text } from '@chakra-ui/react'
+
+const NavBar = () => {
+  return (
+    <Button borderRadius="25px" padding="10px" backgroundColor="blue" color="red" size="xs">
+      <Text fontSize={20}>Hello Thailand</Text>
+    </Button>
+  )
+}
+
+export default NavBar
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import NavBar from "../component/common/Navbar"
+
+const HomePage = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
+}
+
+export default HomePage
