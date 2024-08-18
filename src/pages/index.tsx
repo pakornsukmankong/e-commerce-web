f004b3bfafe41d395c3ca1ced38b56e1b9f79be3
Pakorn Sukmankong <floksong.001@gmail.com>
2024-08-19 00:24:46 +0700

    fix: warning

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Index() {
   return (
-    <>
-      <div>Index page</div>
-    </>
+   <div>
+      <h1>Welcome to my Next.js App</h1>
+   </div>
   )
 }
