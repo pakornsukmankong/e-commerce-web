c46120885f16d980a3f29d45c47b13462be9b65a
Pakorn Sukmankong <floksong.001@gmail.com>
2024-08-20 20:58:42 +0700

    feat: add colors

---
+++ src/pages/component/colors/index.ts
@@ -0,0 +1,11 @@
+export const Colors = {
+    gray1: '#F8F9FA',
+    gray2: '#E9ECEF',
+    gray3: '#DEE2E6',
+    gray4: '#CED4DA',
+    gray5: '#ADB5BD',
+    gray6: '#6C757D',
+    gray7: '#495057',
+    gray8: '#343A40',
+    gray9: '#212529',
+}
